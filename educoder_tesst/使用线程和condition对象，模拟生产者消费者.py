b036b5e373f17c03ca0bf12cd421d8c73688b58d
Noviceprogrammer1101 <283478028@qq.com>
2020-12-16 08:15:28 +0800

    更新educoder_tesst

--- educoder_tesst/使用线程和condition对象，模拟生产者消费者.py
+++ educoder_tesst/使用线程和condition对象，模拟生产者消费者.py
@@ -50,3 +50,56 @@ t1 = threading.Thread(name="A", target=get_monney, args=(A, 'A', '123', '200'))
 t1.start()
 t2 = threading.Thread(name="B", target=put_monney, args=(A, 'B', '123', '200'))
 t2.start()
+
+
+
+#另一个版本
+
+import threading,time
+class Producer(threading.Thread):
+    def __init__(self,threadname):
+
+        threading.Thread.__init__(self,name=threadname)
+
+    def run(self):
+        global count
+        while True:
+            if con.acquire():
+                if count>100:
+                    con.wait()
+
+                else:
+                    count=count+50
+                    print(self.name+' produce 50,count='+str(count))
+                    con.notify()
+
+                con.release()
+
+class Consumer(threading.Thread):
+    def __init__(self,threadingname):
+        threading.Thread.__init__(self,name=threadname)
+    def run(self):
+        global count
+        while True:
+            if count<100:
+                con.wait()
+
+            else:
+                count=count-25
+                print(self.name+' counsumer 25,count='+str(count))
+                con.notify()
+            con.release()
+            time.sleep(1)
+
+count=200
+con=threading.Condition()
+def test():
+    for i in range(2):
+        p=Producer('producer')
+        p.start()
+    for i in range(3):
+        c=Consumer('consumer')
+        c.start()
+
+if __name__=='__main__':
+    test()
